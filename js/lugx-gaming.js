7fbe7902268f6da1352e26645b5d46e031968591
Abdalrhman Mohammed <77964363+AbdalrhmanMohammed@users.noreply.github.com>
2023-07-08 17:42:37 +0300

    Updated 'lugx-gaming.js' file.

--- js/lugx-gaming.js
+++ js/lugx-gaming.js
@@ -1,10 +1,15 @@
 let header = document.querySelector('.header'),
     offsetTop = header.offsetTop;
 
-window.addEventListener("scroll", function (e) {
+function sticky() {
   if (this.scrollY > offsetTop) {
     header.classList.add('sticky');
   } else {
     header.classList.remove('sticky');
   }
-})
+}
+
+sticky();
+
+window.addEventListener("scroll", sticky);
+
